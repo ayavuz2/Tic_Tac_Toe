025db766729d0c652cee98d8e54d460ba36eb91a
ayavuz2 <a.yavuzum@gmail.com>
2020-08-04 06:07:56 +0300

    Added a function that checks the game situation and decide whether its draw or won. message_render function is added. Removed The Player class. It turns out we didnt even need that class from the beginning :). And also some small updates...

--- TicTacToe.py
+++ TicTacToe.py
@@ -26,7 +26,6 @@ class Spot():
 		self.width = GAP
 		self.char = char
 		self.char_label = None
-		print(row, col)
 
 	def fill_char(self, move_count):
 		self.char = 'X' if move_count%2 == 0 else 'O'
@@ -38,10 +37,23 @@ class Spot():
 		return False
 
 	def render_char(self):
-		# WIN.blit(self.char_label, ())
+		if self.char != None:
+			WIN.blit(self.char_label, (self.width*1.5 + self.col*self.width - self.char_label.get_height()//2, 
+				self.width*1.5 + self.row*self.width - self.char_label.get_width()//2))
 
 	def won_by(self):
 		pass
+
+
+def win_or_draw(grid, move_count): # needs adding!
+	for i in range(3):
+		if grid[i][0].char == grid[i][1].char == grid[i][2].char != None:
+			return True
+		elif grid[0][i].char == grid[1][i].char == grid[2][i].char != None:
+			return True
+		elif move_count == 9: 
+			return True
+	return False
 
 
 def get_click_pos(pos, width):
@@ -54,13 +66,14 @@ def get_click_pos(pos, width):
 		return None, None
 
 
-def message_render(message):
-	pass
+def message_render(win, width, message=""):
+	message_label = FONT.render(message, 1, (255,0,0))
+	win.blit(message_label, (width//2 - message_label.get_width()//2, GAP//2 - message_label.get_height()//2))
 
 
 def make_grid(width):
-	grid = [[0 for x in range(3)] for y in range(3)]
-
+	grid = [[x for x in range(3)] for y in range(3)]
+	print(grid)
 	for i in range(3):
 		for j in range(3):
 			spot = Spot(i, j)
@@ -69,7 +82,7 @@ def make_grid(width):
 	return(grid)
 
 
-def draw_grid(win, width):
+def draw_grid(win, grid):
 	# Rows
 	for i in range(2,4):
 		pygame.draw.line(win, BLACK, (GAP, GAP*i), (GAP*4, GAP*i), 2)
@@ -77,10 +90,18 @@ def draw_grid(win, width):
 	for j in range(2,4):
 		pygame.draw.line(win, BLACK, (GAP*j, GAP), (GAP*j, GAP*4), 2)
 
-def draw(win, width):
+	for i in range(3):
+		for j in range(3):
+			grid[i][j].render_char()
+
+
+def draw(win, width, grid, message=""):
 	win.fill((255,255,255))
 
-	draw_grid(win, width)
+	draw_grid(win, grid)
+
+	if message != "":
+		message_render(win, width, message)
 
 	pygame.display.update()
 
@@ -88,10 +109,7 @@ def draw(win, width):
 def main(win, width):
 	grid = make_grid(width)
 	
-	# Make a message function
-	
-	message = ""
-	message_label = main_font.render(message, 1, (255,0,0))
+	message = ""	
 	message_count = 0
 
 	run = True
@@ -104,7 +122,9 @@ def main(win, width):
 
 	while run:
 		clock.tick(FPS)
-		draw(win,width)
+
+		draw(win, width, grid, message)
+
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				run = False
@@ -116,9 +136,21 @@ def main(win, width):
 				if row != None and grid[row][col].is_available():
 					grid[row][col].fill_char(move_count)
 					move_count += 1
+					print(move_count)
 				else:
-					pass
-					
+					message = "Its busy"
+		
+
+		if move_count >= 5:
+			if win_or_draw(grid, move_count):
+				print("O Wins!" if move_count%2 == 0 else "X Wins!")
+				run = False
+
+		if message != "": 
+			message_count += 1
+		if message_count > FPS*3:
+			message_count = 0
+			message = ""
 
 
 	pygame.quit()
